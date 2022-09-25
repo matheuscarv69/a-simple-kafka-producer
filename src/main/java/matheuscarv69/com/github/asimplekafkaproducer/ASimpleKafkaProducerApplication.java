d1535f02b03b493d7eb34587fe159ce37d5e2257
matheuscarv69 <matheus9126@gmail.com9>
2022-09-25 10:46:58 -0400

    :rocket: first commit

---
+++ src/main/java/matheuscarv69/com/github/asimplekafkaproducer/ASimpleKafkaProducerApplication.java
@@ -0,0 +1,13 @@
+package matheuscarv69.com.github.asimplekafkaproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ASimpleKafkaProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ASimpleKafkaProducerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/matheuscarv69/com/github/asimplekafkaproducer/ASimpleKafkaProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package matheuscarv69.com.github.asimplekafkaproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ASimpleKafkaProducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
